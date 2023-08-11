466018887ac8c89c8bfcd0a993cce3acff0af0bc
ggc1412 <ggc1412@gmail.com>
2023-08-11 12:31:10 +0900

    🎨 url checker

--- main.go
+++ main.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/ggc1412/hello-go/myDict"
+	"net/http"
 )
 
+type respResult struct {
+	url        string
+	statusCode int
+}
+
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := myDict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	err := dictionary.Delete(baseWord)
+	var results = make(map[string]int)
+	c := make(chan respResult)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		go hitUrl(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		respResult := <-c
+		results[respResult.url] = respResult.statusCode
+	}
+
+	for url, code := range results {
+		fmt.Println(url, code)
+	}
+}
+
+func hitUrl(url string, c chan<- respResult) {
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(baseWord)
+		fmt.Println(errRequestFailed)
+		resp.StatusCode = 0
 	}
-	err1 := dictionary.Delete(baseWord)
-	if err1 != nil {
-		fmt.Println(err1)
-	} else {
-		fmt.Println(baseWord)
-	}
+	c <- respResult{url: url, statusCode: resp.StatusCode}
 }
